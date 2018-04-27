8395a3eda5e65fd941f25e9bc39ec9c7d4957832
Katharine Abbott <katharinemabbott@gmail.com>
2018-04-26 22:25:28 -0400

    added more js

--- js/index.js
+++ js/index.js
@@ -12,12 +12,27 @@
  var gameState = {
    "player":1,
    "game-board-top-left" : "",
+   "game-board-top-middle" : "",
+   "game-board-top-right" : "",
+   "game-board-middle-left" : "",
+   "game-board-center" : "",
+   "game-board-middle-right" : "",
+   "game-board-bottom-left" : "",
+   "game-board-bottm-middle" : "",
+   "game-board-bottom-right" : "",
  }
 
 
 function draw(currentGame) {
-  // fill the gameboard with X's and O's
-  console.log(currentGame);
+  $(".game-board-top-left").text(gameState["game-board-top-left"])
+  $(".game-board-top-middle").text(gameState["game-board-top-middle"])
+  $(".game-board-top-right").text(gameState["game-board-top-right"])
+  $(".game-board-middle-left").text(gameState["game-board-middle-left"])
+  $(".game-board-center").text(gameState["game-board-center"])
+  $(".game-board-middle-right").text(gameState["game-board-middle-right"])
+  $(".game-board-bottom-left").text(gameState["game-board-bottom-left"])
+  $(".game-board-bottm-middle").text(gameState["game-board-bottm-middle"])
+  $(".game-board-bottom-right").text(gameState["game-board-bottom-right"])
 }
 
 
@@ -40,14 +55,46 @@ function draw(currentGame) {
 function update(player, cell){
   //1. update the board state to be the correct piece
   //2. change the player
+  gameState[cell] = "X"
 }
 
 
 function setup() {
 
-
   $(".game-board-top-left").click(function() {
     update(gameState["player"],"game-board-top-left");
+    draw(gameState);
+  });
+  $(".game-board-top-middle").click(function() {
+    update(gameState["player"],"game-board-top-middle");
+    draw(gameState);
+  });
+  $(".game-board-top-right").click(function() {
+    update(gameState["player"],"game-board-top-right");
+    draw(gameState);
+  });
+  $(".game-board-middle-left").click(function() {
+    update(gameState["player"],"game-board-middle-left");
+    draw(gameState);
+  });
+  $(".game-board-center").click(function() {
+    update(gameState["player"],"game-board-center");
+    draw(gameState);
+  });
+  $(".game-board-middle-right").click(function() {
+    update(gameState["player"],"game-board-middle-right");
+    draw(gameState);
+  });
+  $(".game-board-bottom-left").click(function() {
+    update(gameState["player"],"game-board-bottom-left");
+    draw(gameState);
+  });
+  $(".game-board-bottom-middle").click(function() {
+    update(gameState["player"],"game-board-bottom-middle");
+    draw(gameState);
+  });
+  $(".game-board-bottom-right").click(function() {
+    update(gameState["player"],"game-board-bottom-right");
     draw(gameState);
   });
 
